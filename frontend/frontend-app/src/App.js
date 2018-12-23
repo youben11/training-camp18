45a77d228a87bd779b193505c1e92cc05ec2006c
ines <fnsines@gmail.com>
2018-12-23 17:45:36 +0000

    a lot of work

--- frontend/frontend-app/src/App.js
+++ frontend/frontend-app/src/App.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-
+import './assets/css/uikit.css';
+import Login from './components/login'
+import Header from './components/header'
+import Social from './components/social/Social'
+import Bg from './assets/icons/bg.jpg'
+import Nav from './components/nav/Nav'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div   style={{backgroundImage:'url(${Bg})',height:"660px"}} className="App">
+      <Header/>
+       <Login/>
+       <Social></Social>
+       <Nav></Nav>
       </div>
     );
   }
---
+++ frontend/frontend-app/src/components/nav/Nav.js
@@ -0,0 +1,41 @@
+import React ,{Component} from 'react'
+
+
+class Nav extends Component{
+ render(){
+
+    return(
+        <nav class="uk-navbar-container uk-margin" uk-navbar>
+        <div class="uk-navbar-left">
+    
+            <a class="uk-navbar-item uk-logo" href="#">Logo</a>
+    
+            <ul class="uk-navbar-nav">
+                <li>
+                    <a href="#">
+                        <span class="uk-icon uk-margin-small-right" uk-icon="icon: star"></span>
+                        Features
+                    </a>
+                </li>
+            </ul>
+    
+            <div class="uk-navbar-item">
+                <div>Some <a href="#">Link</a></div>
+            </div>
+    
+            <div class="uk-navbar-item">
+                <form action="javascript:void(0)">
+                    <input class="uk-input uk-form-width-small" type="text" placeholder="Input">
+                    <button class="uk-button uk-button-default">Button</button>
+                </form>
+            </div>
+    
+        </div>
+    </nav>  
+
+
+ }
+}
+
+
+export default App;
